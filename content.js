a2c60d72b3e910a5bcd19f6b3c0991ae286d5192
Santiago Velasquez <santiagov801@gmail.com>
2024-08-19 19:14:17 -0500

    clear & close btns

--- content.js
+++ content.js
@@ -3,9 +3,18 @@ if (root) {
   const consoleContainer = document.createElement('div');
   consoleContainer.classList.add('overlay');
 
-  const header = document.createElement('h1');
-  header.textContent = 'Logging Console';
+  const header = document.createElement('div');
+  // header.textContent = 'Logging Console';
   header.classList.add('console-header');
+  header.innerHTML = `
+    <div class="console-header-title">Logging Console</div>
+    <div title="Clear" class="clear-console-btn action-btn" onclick="document.querySelector('.console-logs').innerHTML = ''">
+      ${TrashIcon()}
+    </div>
+    <div title="Close" class="close-console-btn action-btn" onclick="document.querySelector('.overlay').style.display = 'none'">
+      ${XIcon()}
+    </div>
+  `;
 
   const logs = document.createElement('div');
   logs.classList.add('console-logs');
@@ -13,6 +22,7 @@ if (root) {
   consoleContainer.appendChild(header);
   consoleContainer.appendChild(logs);
   root.appendChild(consoleContainer);
+  // root.appendChild(createBubbleButton(consoleContainer));
 }
 
 // jsonViewer.showJSON(jsonData);
@@ -102,4 +112,58 @@ function createJsonLog(jsonData, container) {
   var jsonViewer = new JSONViewer();
   jsonViewer.showJSON(jsonData, 10, 1);
   container.appendChild(jsonViewer.getContainer());
+}
+
+function TrashIcon() {
+  return `
+    <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'>
+    <path d='M3 6h18' />
+    <path d='M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6' />
+    <path d='M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2' />
+    </svg>
+    `;
+}
+
+function XIcon() {
+  return `
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'>
+      <path d='M18 6 6 18' />
+      <path d='m6 6 12 12' />
+    </svg>
+    `;
+}
+
+function createBubbleButton(overlay) {
+  const button = document.createElement('button');
+  button.classList.add('bubble-button');
+  if (overlay.style.display === 'none') {
+    button.classList.add('bubble-button-hidden');
+  } else {
+    button.classList.remove('bubble-button-hidden');
+  }
+  button.onclick = function () {
+    if (overlay) {
+      overlay.style.display =
+        overlay.style.display === 'none' ? 'block' : 'none';
+    }
+  };
+  return button;
 }
